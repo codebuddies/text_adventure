2b4e54fd53607e53814e54a17d2b6a0350d19f4a
Will Wilson <williamrwilson@gmail.com>
2017-05-28 20:01:57 -0400

    first attempt at solution

--- text_adventure/entity.py
+++ text_adventure/entity.py
@@ -21,23 +21,58 @@ class Entity:
 
 class MyCharacter(Entity):
 
+    TOO_THIRSTY = 2
+    TOO_TIRED = 5
+
     def __init__(self, name):
-        # Something needed??
-        pass
+        Entity.__init__(self, name)
+        self.status = "working"
+        self.thirst_counter = 1
+        self.sleep_counter = 1
 
     def update(self):
-        self.tiredness += 1
-        
-        self.thirst += 1 # Should only be called when not sleeping...
+        self.check_status()
+        self.check_thirst()
+        self.check_tiredness()
+        self.thirst += self.thirst_counter
+        self.tiredness += self.sleep_counter
+        print(f"{self.name} is {self.status}")
+        print(f"Tiredness: {self.tiredness}")
+        print(f"Thirst: {self.thirst}")
 
-        print("Show what your character is currently doing.")
-        print("If they get thirsty, they should leave and go get a drink.")
-        print("Only once they get to the bar, they have the opportunity "
-              " to drink, which happens with the next update call.")
-        print("Once they're done drinking, their thirst should go back to 0.")
-        print("When characters are tired, they should head off back home.")
-        print("Once home, they don't get thirsty anymore. They loose 3"
-              " tiredness at every update call.")
-        print("Once rested, they can go back to work.")
+    def check_status(self):
+        if self.status == "working":
+            self.working()
+        if self.status == "sleeping":
+            self.sleeping()
+        if self.status == "at the bar":
+            self.at_the_bar()
 
+    def check_thirst(self):
+        if self.thirst >= self.TOO_THIRSTY:
+            self.status = "at the bar"
+            print("I'm thirsty! Time for a drink!")
 
+    def check_tiredness(self):
+        if self.tiredness >= self.TOO_TIRED:
+            self.status = "sleeping"
+            print("YAWN! I'm going home!")
+
+    def working(self):
+        self.thirst_counter = 1
+        self.sleep_counter = 1
+
+    def sleeping(self):
+        self.thirst_counter = 0
+        self.sleep_counter = -3
+        if self.tiredness <= 0:
+            self.sleep_counter = 1
+            self.thirst_counter = 1
+            self.status = "working"
+
+    def at_the_bar(self):
+        self.thirst_counter = -3
+        self.sleep_counter = 1
+        if self.thirst <= 0:
+            self.thirst_counter = 1
+            self.status = "working"
