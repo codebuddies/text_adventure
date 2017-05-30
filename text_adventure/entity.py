1e9e5bc45ddee1b55be82dc2cb85177a8b48eb9e
Daniel <dan.gillet737@gmail.com>
2017-05-30 17:42:06 +0200

    Usage of state machine pattern

--- text_adventure/entity.py
+++ text_adventure/entity.py
@@ -19,25 +19,77 @@ class Entity:
     def update(self):
         raise NotImplementedError
 
+
 class MyCharacter(Entity):
 
     def __init__(self, name):
-        # Something needed??
-        pass
+        super().__init__(name)
+        self.state = Working()
+
+    def change_state(self, new_state):
+        self.state.exit(self)
+        self.state = new_state
+        self.state.enter(self)
 
     def update(self):
-        self.tiredness += 1
+        self.state.update(self)
+
+
+class Working:
+    def enter(self, entity):
+        print("Let's get some work done!")
+
+    def exit(self, entity):
+        print("Leaving the office.")
+
+    def update(self, entity):
+        entity.tiredness += 1
+        entity.thirst += 1
+
+        print("Working my ass off.")
+
+        if entity.thirst > entity.TOO_THIRSTY:
+            print("I'm too thirsty. Heading off to the bar!")
+            entity.change_state(Drinking())
+        elif entity.tiredness > entity.TOO_TIRED:
+            print("I'm too tired to keep on working. I'll go back home get"
+                  " some sleep.")
+            entity.change_state(Sleeping())
+
+class Drinking:
+    def enter(self, entity):
+        print("Let's checkout this bar. They seem to have tap beers.")
+
+    def exit(self, entity):
+        print("Let's leave a tip before leaving the bar.")
+
+    def update(self, entity):
+        entity.tiredness += 1
+
+        print("Drinking a nice beer")
+        entity.thirst = 0
+
+        if entity.tiredness > entity.TOO_TIRED:
+            print("Drinking made me tired. I'll go back home get"
+                  " some sleep.")
+            entity.change_state(Sleeping())
+        else:
+            print("Now I can go back to work.")
+            entity.change_state(Working())
+
+class Sleeping:
+    def enter(self, entity):
+        print("Home, sweet home.")
+
+    def exit(self, entity):
+        print("Let's lock the front door before leaving home.")
+
+    def update(self, entity):
+        entity.tiredness += 1
         
-        self.thirst += 1 # Should only be called when not sleeping...
-
-        print("Show what your character is currently doing.")
-        print("If they get thirsty, they should leave and go get a drink.")
-        print("Only once they get to the bar, they have the opportunity "
-              " to drink, which happens with the next update call.")
-        print("Once they're done drinking, their thirst should go back to 0.")
-        print("When characters are tired, they should head off back home.")
-        print("Once home, they don't get thirsty anymore. They loose 3"
-              " tiredness at every update call.")
-        print("Once rested, they can go back to work.")
-
-
+        print("Zzzz")
+        entity.tiredness -= 3
+        if entity.tiredness <= 0:
+            entity.tiredness = 0
+            print("I had enough sleep. I'll go to work.")
+            entity.change_state(Working())
--- text_adventure/game.py
+++ text_adventure/game.py
@@ -11,7 +11,7 @@ def run():
     world = World()
 
     # Instantiate your character here and add it to the world:
-    character = MyCharacter("My Name")
+    character = MyCharacter("Dan")
     world.add_entity(character)
 
     # We advance the world 20 timesteps
