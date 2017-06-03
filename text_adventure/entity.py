ff070fd4f3cff3040ccf395d45712c5eb18797a1
lpatmo <linda.peng@alumni.duke.edu>
2017-06-03 00:04:22 -0700

    clear out unnecessary comments

--- text_adventure/entity.py
+++ text_adventure/entity.py
@@ -61,32 +61,6 @@ class MyCharacter(Entity):
 
 
 
-    # def get_a_drink(self):
-    #     print("Heading off to the bar!")
-    #     if self.thirst > 1:
-    #         self.thirst -= 1
 
-    # def go_to_work(self):
-    #     print("I had enough sleep. I'll go to work.")
-    #     self.tiredness += 1
-    #     self.thirst += 1
-
-
-    # def sleep(self):
-    #     print("Drinking made me tired. I'll go back home get some sleep.")
-    #     self.thirst = 0
-    #     self.tiredness = 0
-
-
-
-        # print("Show what your character is currently doing.")
-        # print("If they get thirsty, they should leave and go get a drink.")
-        # print("Only once they get to the bar, they have the opportunity "
-        #       " to drink, which happens with the next update call.")
-        # print("Once they're done drinking, their thirst should go back to 0.")
-        # print("When characters are tired, they should head off back home.")
-        # print("Once home, they don't get thirsty anymore. They lose 3"
-        #       " tiredness at every update call.")
-        # print("Once rested, they can go back to work.")
 
 
